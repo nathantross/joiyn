75119c51d033a8c8a9379bae01bdf1c464dce354
nathantross <nathantross@gmail.com>
2014-02-08 16:18:42 -0800

    Created Joiyn App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JoiynApp::Application.config.secret_key_base = '69e3e0d1708160b16302307e514dec2b037cda2fa587fc69940c96d77c7b56175c28a26d5cc9dfc08bdcab099f20395dbe9121f33eafa751da86c3c899435dac'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JoiynApp::Application.config.session_store :cookie_store, key: '_joiyn_app_session'
